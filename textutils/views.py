23ab4bde77b3afd890bd5c95f585e2d0bcf561c3
Rushi <hrushikeshborakhede1089@gmail.com>
2020-07-18 12:02:27 +0530

    modified:   templates/index.html 	modified:   textutils/views.py

--- textutils/views.py
+++ textutils/views.py
@@ -1,4 +1,4 @@
-# I have created this file - Harry
+# This is harry bhai ka code
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -17,10 +17,12 @@ def analyze(request):
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
     numberremover = request.POST.get('numberremover','off')
+    CharType = request.POST.get('CharType','off')
+    SenType = request.POST.get('SenType','off')
 
     #Check which checkbox is on
     if removepunc == "on":
-        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        punctuations = '''!|()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
@@ -58,6 +60,7 @@ def analyze(request):
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
     
     if (numberremover == "on"):
         analyzed = ""
@@ -66,13 +69,60 @@ def analyze(request):
         for char in djtext:
             if char not in numbers:
                 analyzed = analyzed + char
-        
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
         djtext = analyzed
 
+# Below this is my code
+    if(CharType=="on"):
+        djtext = djtext
+        analyzed = ""
+        capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        small = 'abcdefghijklmnopqrstuvwxyz'
+        symbol = '''!()-[]|{};:'"\,<>./?@#$%^&*_~'''
+        capitalcount = 0
+        smallcount = 0
+        symbolcount = 0
+        blankcount = 0
+        for char in djtext:
+            if char in capital:
+                capitalcount +=1
+            if char in small:
+                smallcount +=1
+            if char in symbol:
+                symbolcount +=1
+            if char == ' ':
+                blankcount += 1
+        
+        analyzed = f"The give text contains {capitalcount}  capital letters.\nThe give text contains {smallcount} small letters.\nThe give text contains {symbolcount} symbol.\nThe give text contains {blankcount} blanks.\n"
+        params = {'purpose': 'Character Type counter', 'analyzed_text': analyzed}
+        djtext = analyzed
     
-    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on"):
-        return HttpResponse("please select any operation and try again")
+    if SenType == "on":
+        analyzed = ""
+        asserativecount = 0
+        exclamatorycount = 0
+        questioncount = 0
+        for char in djtext:
+            if char == '.':
+                asserativecount +=1
+            if char == '!':
+                exclamatorycount +=1
+            if char == '?':
+                questioncount +=1
+
+        analyzed = f"The given text contains {asserativecount} asserative sentences.\nThe given text contains {exclamatorycount} exclamatory sentences.\nThe given text contains {questioncount} interrorgative sentences.\n"
+        params = {'purpose': 'Sentence Type Counter', 'analyzed_text': analyzed}
+        djtext = analyzed
+    
+# to this is mine code
+
+    
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on" and CharType!='on' and SenType!='on'):
+        return HttpResponse('please select any operation and try again')
+
+    
+
+    
 
     return render(request, 'analyze.html', params)
 
