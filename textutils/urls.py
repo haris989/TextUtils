c6882e26181dc272e61df13c1de5626c614ccd13
Praddyumn Yadav <praddyumny@gmail.com>
2023-01-13 21:48:18 +0530

    Formatted The Code a Little Bit

--- textutils/urls.py
+++ textutils/urls.py
@@ -18,9 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('analyze', views.analyze, name='analyze'),
-    path('about', views.about, name='about')
-
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("analyze", views.analyze, name="analyze"),
+    path("about", views.about, name="about"),
 ]
